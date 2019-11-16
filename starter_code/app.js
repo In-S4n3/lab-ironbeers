9d8d6d3cfcae001e20f8fb6b2e476ba74f3b2f99
In-S4n3 <tiagodcpp@gmail.com>
2019-11-16 15:32:22 +0000

    wip

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,11 +10,27 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers);
+    res.render('beers', {beers: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/randomBeer', (req, res, next) => {
+  res.render('randomBeer');
+});
+
 
 
 
